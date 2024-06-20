3b06021eb08a666538fccd2d067412e4bdd8012d
HarshaVardhanRajuK <rajharsha666@gmail.com>
2024-06-20 14:51:01 +0530

    Save to Local storage

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,25 @@ import { v4 as uuidv4 } from "uuid";
 import { MdEditDocument } from "react-icons/md";
 import { RiDeleteBin6Fill } from "react-icons/ri";
 
-
 import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   let [todo, setTodo] = useState("");
 
-  let [todos, setTodos] = useState([]);
+  // let [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
 
-  let [showFinished, setShowFinished] = useState(true)
+  let [showFinished, setShowFinished] = useState(true);
+
+  
 
   useEffect(() => {
-    let todos = JSON.parse(localStorage.getItem("todos"));
-
-    if (todos) {
-      setTodos(todos);
-    }
-  }, []);
-
-
-
-  function saveToLocal() {
     localStorage.setItem("todos", JSON.stringify(todos));
-    console.log(todos)
-  }
+  }, [todos]);
+
 
   function handleSave() {
     if (todo.length < 3) {
@@ -38,7 +33,6 @@ const App = () => {
     setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
 
     setTodo("");
-    saveToLocal();
   }
 
   function handlecheckbox(e) {
@@ -49,16 +43,13 @@ const App = () => {
     newTodos[index].isCompleted = !newTodos[index].isCompleted;
 
     setTodos(newTodos);
-    saveToLocal();
   }
 
   function handleDelete(id) {
     let newTodos = todos.filter((i) => i.id !== id);
 
-    console.log(newTodos)
 
     setTodos(newTodos);
-    saveToLocal();
   }
 
   function handleEdit(id) {
@@ -69,10 +60,7 @@ const App = () => {
     let newTodos = todos.filter((i) => i.id !== id);
 
     setTodos(newTodos);
-    saveToLocal();
   }
-
-  
 
   return (
     <div className="min-h-[100vh] bg-[#9AC8CD]">
@@ -88,13 +76,13 @@ const App = () => {
             type="text"
             value={todo}
             className="font-[500] w-[270px] md:w-[500px] p-2 rounded-md focus:outline-blue-600 mr-1 text-center"
-            placeholder="Create Your Task"
-            // onKeyUp={(e)=>{
-            //   if (e.key === "Enter") {
-            //     handleSave()
-                
-            //   }
-            // }}
+            placeholder="Create Your Task, press ↵ or save"
+            onKeyUp={(e)=>{
+              if (e.key === "Enter") {
+                handleSave()
+
+              }
+            }}
           />
           <button
             onClick={handleSave}
@@ -103,76 +91,89 @@ const App = () => {
             Save
           </button>
         </div>
-        <div className="card flex flex-col items-center min-h-[300px] bg-[#0E46A3] w-full md:w-[60%] py-4 backdrop-blur-md border-2 border-blue-300 rounded-lg">
-          <div className="flex mb-2">
-            <input
-              onChange={()=>{setShowFinished(!showFinished)}}
-              type="checkbox"
-              name=""
-              id="showFinished"
-              className="mr-2 cursor-pointer w-6"
-              checked={showFinished}
-            />
-            <label
-              className="text-lg cursor-pointer font-medium text-white"
-              htmlFor="showFinished"
-            >
-              Show Completed
-            </label>
-            
-          </div>
-          <hr />
-          {todos.map((item) => {
-            return ( ( showFinished || !item.isCompleted ) &&
-              <div
-                key={item.id}
-                className="todo p-2 w-[80%] bg-[#E1F7F5] flex justify-between my-1 rounded-md"
-              >
-                <div className="flex">
-                  <input
-                  id={item.id}
-                    name={item.id}
-                    type="checkbox"
-                    checked={item.isCompleted}
-                    onChange={(e) => {
-                      handlecheckbox(e);
-                    }}
-                    className="cursor-pointer w-5"
-                  />
-
-                  <label
-                  htmlFor={item.id}
-                    className={
-                      item.isCompleted
-                        ? "ml-2 flex justify-center items-center text-lg font-semibold line-through"
-                        : "ml-2 flex justify-center items-center text-lg font-semibold"
-                    }
-                  >
-                    {item.todo}
-                  </label>
-                </div>
-
-                <div>
-                  <button
-                    name={item.id}
-                    onClick={() => {
-                      handleEdit(item.id);
-                    }}
-                    className="bg-slate-500 p-2 mr-2 rounded-md font-semibold text-white"
-                  >
-                    <MdEditDocument />
-                  </button>
-                  <button
-                    name={item.id}
-                    onClick={(e) => {
-                      handleDelete(item.id);
-                    }}
-                    className="bg-red-500 p-2 rounded-md font-semibold text-white"
-                  >
-                    <RiDeleteBin6Fill />
-                  </button>
-                </div>
+        <div className="card flex flex-col items-center min-h-[300px] bg-[#0E46A3] w-full md:w-[60%] py-6 backdrop-blur-md border-2 border-blue-300 rounded-lg">
+          {todos.length !== 0 && (
+            <>
+              <div className="flex mb-3">
+                <input
+                  onChange={() => {
+                    setShowFinished(!showFinished);
+                  }}
+                  type="checkbox"
+                  name=""
+                  id="showFinished"
+                  className="mr-2 cursor-pointer w-6"
+                  checked={showFinished}
+                />
+                <label
+                  className="text-lg cursor-pointer font-medium text-white"
+                  htmlFor="showFinished"
+                >
+                  Show Completed
+                </label>
               </div>
+              <div className="h-[1px] w-[90%] bg-gray-200 mb-3"></div>{" "}
+            </>
+          )}
+          {todos.length === 0 && (
+            <div className="text-slate-400 p-2 mt-4 text-center text-xl">
+              {" "}
+              No Tasks! Add some to appear here
+            </div>
+          )}
+          {todos.map((item) => {
+            return (
+              (showFinished || !item.isCompleted) && (
+                <div
+                  key={item.id}
+                  className="todo p-2 w-[80%] bg-[#E1F7F5] flex justify-between my-1 rounded-md"
+                >
+                  <div className="flex">
+                    <input
+                      id={item.id}
+                      name={item.id}
+                      type="checkbox"
+                      checked={item.isCompleted}
+                      onChange={(e) => {
+                        handlecheckbox(e);
+                      }}
+                      className="cursor-pointer w-5"
+                    />
+
+                    <label
+                      htmlFor={item.id}
+                      className={
+                        item.isCompleted
+                          ? "ml-2 flex justify-center items-center text-lg font-semibold line-through"
+                          : "ml-2 flex justify-center items-center text-lg font-semibold"
+                      }
+                    >
+                      {item.todo}
+                    </label>
+                  </div>
+
+                  <div>
+                    <button
+                      name={item.id}
+                      onClick={() => {
+                        handleEdit(item.id);
+                      }}
+                      className="bg-slate-500 p-2 mr-2 rounded-md font-semibold text-white"
+                    >
+                      <MdEditDocument />
+                    </button>
+                    <button
+                      name={item.id}
+                      onClick={(e) => {
+                        handleDelete(item.id);
+                      }}
+                      className="bg-red-500 p-2 rounded-md font-semibold text-white"
+                    >
+                      <RiDeleteBin6Fill />
+                    </button>
+                  </div>
+                </div>
+              )
             );
           })}
         </div>
