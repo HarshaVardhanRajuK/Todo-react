46df32ff909c2d29048274a7c326ced54fa38343
HarshaVardhanRajuK <rajharsha666@gmail.com>
2024-06-20 13:45:39 +0530

    Added responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 import { v4 as uuidv4 } from "uuid";
+import { MdEditDocument } from "react-icons/md";
+import { RiDeleteBin6Fill } from "react-icons/ri";
+
 
 import Navbar from "./components/Navbar/Navbar";
 
@@ -10,15 +13,22 @@ const App = () => {
 
   let [todos, setTodos] = useState([]);
 
-  useEffect(()=>{
-    
-    let todos = JSON.parse(localStorage.getItem("todos"))
-    console.log(todos)
-    if(!todos){
-      return
+  let [showFinished, setShowFinished] = useState(true)
+
+  useEffect(() => {
+    let todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos) {
+      setTodos(todos);
     }
-    setTodos(todos)
-  },[])
+  }, []);
+
+
+
+  function saveToLocal() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+    console.log(todos)
+  }
 
   function handleSave() {
     if (todo.length < 3) {
@@ -28,51 +38,48 @@ const App = () => {
     setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
 
     setTodo("");
-    saveToLocal()
+    saveToLocal();
   }
 
   function handlecheckbox(e) {
-    let index = todos.findIndex(i=>i.id === e.target.name)
+    let index = todos.findIndex((i) => i.id === e.target.name);
 
-    let newTodos = [...todos]
+    let newTodos = [...todos];
 
-    newTodos[index].isCompleted = !newTodos[index].isCompleted
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
 
-    setTodos(newTodos)
-    saveToLocal()
-
+    setTodos(newTodos);
+    saveToLocal();
   }
 
-  function handleDelete(e) {
-    let newTodos = todos.filter(i=>i.id!==e.target.name)
+  function handleDelete(id) {
+    let newTodos = todos.filter((i) => i.id !== id);
 
-    setTodos(newTodos)
-    saveToLocal()
+    console.log(newTodos)
+
+    setTodos(newTodos);
+    saveToLocal();
   }
 
   function handleEdit(id) {
-    todos.find(i=>i.id===id)
-    console.log(todos.find(i=>i.id===id).todo)
-    
-    setTodo(todos.find(i=>i.id===id).todo)
+    todos.find((i) => i.id === id);
 
-    let newTodos = todos.filter(i=>i.id!==id)
+    setTodo(todos.find((i) => i.id === id).todo);
 
-    setTodos(newTodos)
-    saveToLocal()
+    let newTodos = todos.filter((i) => i.id !== id);
 
+    setTodos(newTodos);
+    saveToLocal();
   }
 
-  function saveToLocal() {
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }
+  
 
   return (
-    <div className="min-h-[100vh] bg-sky-200">
+    <div className="min-h-[100vh] bg-[#9AC8CD]">
       <Navbar />
-      <div className="min-h-[88vh] flex flex-col items-center gap-12 p-10">
-        <h1 className="text-4xl font-bold">Add your TODOs</h1>
-        <div>
+      <div className="min-h-[88vh] flex flex-col items-center gap-8 p-10">
+        <h1 className="text-2xl md:text-4xl font-bold">Add Your TODOs</h1>
+        <div className="flex">
           <input
             onChange={(e) => {
               setTodo(e.target.value);
@@ -80,49 +87,89 @@ const App = () => {
             id="userinput"
             type="text"
             value={todo}
-            className="font-[500] w-[500px] p-2 rounded-md focus:outline-blue-600 mr-1 text-center"
+            className="font-[500] w-[270px] md:w-[500px] p-2 rounded-md focus:outline-blue-600 mr-1 text-center"
+            placeholder="Create Your Task"
+            // onKeyUp={(e)=>{
+            //   if (e.key === "Enter") {
+            //     handleSave()
+                
+            //   }
+            // }}
           />
           <button
             onClick={handleSave}
-            className="bg-blue-600 text-white px-4 py-2 rounded-md"
+            className="bg-blue-600 text-white px-4 py-2 rounded-md font-bold"
           >
             Save
           </button>
         </div>
-        <div className="card flex flex-col items-center min-h-[300px] bg-green-300 w-3/4 pt-4">
+        <div className="card flex flex-col items-center min-h-[300px] bg-[#0E46A3] w-full md:w-[60%] py-4 backdrop-blur-md border-2 border-blue-300 rounded-lg">
+          <div className="flex mb-2">
+            <input
+              onChange={()=>{setShowFinished(!showFinished)}}
+              type="checkbox"
+              name=""
+              id="showFinished"
+              className="mr-2 cursor-pointer w-6"
+              checked={showFinished}
+            />
+            <label
+              className="text-lg cursor-pointer font-medium text-white"
+              htmlFor="showFinished"
+            >
+              Show Completed
+            </label>
+            
+          </div>
+          <hr />
           {todos.map((item) => {
-            return (
+            return ( ( showFinished || !item.isCompleted ) &&
               <div
                 key={item.id}
-                className="todo p-2 w-[80%] bg-blue-400 flex justify-between"
+                className="todo p-2 w-[80%] bg-[#E1F7F5] flex justify-between my-1 rounded-md"
               >
                 <div className="flex">
                   <input
-                  name={item.id}
+                  id={item.id}
+                    name={item.id}
                     type="checkbox"
-                    value={item.isCompleted}
+                    checked={item.isCompleted}
                     onChange={(e) => {
                       handlecheckbox(e);
                     }}
+                    className="cursor-pointer w-5"
                   />
 
-                  <div
+                  <label
+                  htmlFor={item.id}
                     className={
                       item.isCompleted
-                        ? "bg-green-200 ml-2 flex justify-center items-center line-through"
-                        : "bg-green-200 ml-2 flex justify-center items-center"
+                        ? "ml-2 flex justify-center items-center text-lg font-semibold line-through"
+                        : "ml-2 flex justify-center items-center text-lg font-semibold"
                     }
                   >
                     {item.todo}
-                  </div>
+                  </label>
                 </div>
 
                 <div>
-                  <button name={item.id} onClick={() => {handleEdit(item.id)}} className="bg-slate-500 px-3 py-1 mr-2 rounded-md">
-                    Edit
+                  <button
+                    name={item.id}
+                    onClick={() => {
+                      handleEdit(item.id);
+                    }}
+                    className="bg-slate-500 p-2 mr-2 rounded-md font-semibold text-white"
+                  >
+                    <MdEditDocument />
                   </button>
-                  <button name={item.id} onClick={(e) => {handleDelete(e)}} className="bg-red-500 px-3 py-1 rounded-md">
-                    Delete
+                  <button
+                    name={item.id}
+                    onClick={(e) => {
+                      handleDelete(item.id);
+                    }}
+                    className="bg-red-500 p-2 rounded-md font-semibold text-white"
+                  >
+                    <RiDeleteBin6Fill />
                   </button>
                 </div>
               </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-around items-center h-16 bg-blue-500 text-white">
-      <div className="logo text-2xl font-bold">Todo</div>
-      <ul className="nav-right flex gap-20 text-xl">
+    <nav className="flex justify-around items-center h-20 bg-[#1E0342] text-white">
+      <div className="logo text-2xl font-bold">iTask - <span className="text-xl font-semibold">Manage Your Tasks</span></div>
+      {/* <ul className="nav-right flex gap-20 text-xl">
         <li className="hover:font-bold cursor-pointer transition-all">Home</li>
         <li className="hover:font-bold cursor-pointer transition-all">About</li>
-      </ul>
+      </ul> */}
     </nav>
   );
 };
