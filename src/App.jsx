3dbe79019620ea1a909e3dadf422cfc90ce64622
HarshaVardhanRajuK <rajharsha666@gmail.com>
2024-06-21 02:09:59 +0530

    Dates shown

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ import Footer from "./components/Footer/Footer";
 const App = () => {
   let [todo, setTodo] = useState("");
 
-  // let [todos, setTodos] = useState([]);
+  let [duedate, setDuedate] = useState("");
+
   const [todos, setTodos] = useState(() => {
     const savedTodos = localStorage.getItem("todos");
     return savedTodos ? JSON.parse(savedTodos) : [];
@@ -19,22 +20,38 @@ const App = () => {
 
   let [showFinished, setShowFinished] = useState(true);
 
-  
+  let [error, setError] = useState(false);
+
+  // useEffect(() => {
+  //   if (Notification.permission === "denied") {
+  //     Notification.requestPermission().then((res) => console.log(res));
+  //   }
+  // }, []);
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
+
+    // todos.forEach((todo) => {
+    //   let [date, time] = todo.duedate.split("T");
+
+    //   let [y, M, d] = date.split("-");
+    //   let [h, m] = time.split(":");
+
+    //   let now = new Date();
+
+    //   let givendate = new Date(y, M - 1, d, h, m);
+
+    //   let dueInMilli = givendate - now;
+
+    //   if (dueInMilli < 0) {
+    //     return;
+    //   }
+
+    //   setTimeout(() => {
+    //     new Notification("hello");
+    //   }, dueInMilli);
+    // });
   }, [todos]);
-
-
-  function handleSave() {
-    if (todo.length < 3) {
-      return;
-    }
-
-    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
-
-    setTodo("");
-  }
 
   function handlecheckbox(e) {
     let index = todos.findIndex((i) => i.id === e.target.name);
@@ -49,7 +66,6 @@ const App = () => {
   function handleDelete(id) {
     let newTodos = todos.filter((i) => i.id !== id);
 
-
     setTodos(newTodos);
   }
 
@@ -63,12 +79,53 @@ const App = () => {
     setTodos(newTodos);
   }
 
+  function handleSave() {
+    if (todo.length < 3) {
+      return;
+    }
+
+    let createdAt = new Date();
+
+    if (duedate !== "") {
+      let [date, time] = duedate.split("T");
+
+      let [y, M, d] = date.split("-");
+      let [h, m] = time.split(":");
+
+      let givendate = new Date(y, M - 1, d, h, m);
+
+
+      if (givendate - createdAt < 0) {
+        setError(true);
+        return;
+      }
+    }
+
+    createdAt = createdAt.toLocaleString();
+
+    setError(false);
+
+    setTodos([
+      ...todos,
+      { id: uuidv4(), todo, createdAt, duedate, isCompleted: false },
+    ]);
+
+    setTodo("");
+    setDuedate("");
+  }
+
+  function handleDateChange(e) {
+    let userdate = e.target.value;
+
+    setDuedate(userdate);
+  }
+
   return (
     <div className="min-h-[100vh] bg-[#9AC8CD]">
       <Navbar />
-      <div className="min-h-[88vh] flex flex-col items-center gap-8 py-10 px-2">
-        <h1 className="text-2xl md:text-4xl font-bold">Add Your TODOs</h1>
-        <div className="flex">
+      <div className="min-h-[88vh] flex flex-col items-center py-10 px-2">
+        <h1 className="text-2xl md:text-3xl font-bold mb-4">Add Your TODOs</h1>
+        <div className="flex mb-2">
           <input
             onChange={(e) => {
               setTodo(e.target.value);
@@ -76,23 +133,46 @@ const App = () => {
             id="userinput"
             type="text"
             value={todo}
-            className="font-[500] w-[280px] md:w-[500px] p-2 rounded-md focus:outline-blue-600 mr-1 text-center"
+            className="font-[500] w-[360px] md:w-[500px] p-2 rounded-md focus:outline-blue-600 mr-1 text-center"
             placeholder="Create Your Task, press ↵ or save"
-            onKeyUp={(e)=>{
+            onKeyUp={(e) => {
               if (e.key === "Enter") {
-                handleSave()
-
+                handleSave();
               }
             }}
           />
-          <button
-            onClick={handleSave}
-            className="bg-blue-600 text-white px-4 py-2 rounded-md font-bold"
-          >
-            Save
-          </button>
         </div>
+        <div className="flex items-center mb-2">
+          <label
+            htmlFor=""
+            className="mr-2 bg-slate-400 rounded-md text-[12px] text-white p-1"
+          >
+            Due date:{" "}
+          </label>
+          <input
+            className="mr-1 rounded-sm text-[14px] px-2"
+            type="datetime-local"
+            name=""
+            id=""
+            value={duedate}
+            onChange={(e) => {
+              handleDateChange(e);
+            }}
+          />
+        </div>
+        <button
+          onClick={handleSave}
+          className="bg-blue-600 text-white px-4 py-2 mb-2 rounded-md font-bold"
+        >
+          Save
+        </button>
         <div className="card flex flex-col items-center min-h-[300px] bg-[#0E46A3] w-full md:w-[60%] py-6 backdrop-blur-md border-2 border-blue-300 rounded-lg">
+          <span
+            className={error ? "text-red-500 inline" : "text-red-500 hidden"}
+          >
+            {" "}
+            * Enter a valid Date
+          </span>
           {todos.length !== 0 && (
             <>
               <div className="flex mb-3">
@@ -113,12 +193,11 @@ const App = () => {
                   Show Completed
                 </label>
               </div>
-              <div className="h-[1px] w-[90%] bg-gray-200 mb-3"></div>{" "}
+              <div className="h-[1px] w-[90%] bg-gray-200 mb-3"></div>
             </>
           )}
           {todos.length === 0 && (
             <div className="text-slate-400 p-2 mt-4 text-center text-xl">
-              {" "}
               No Tasks! Add some to appear here
             </div>
           )}
@@ -127,51 +206,67 @@ const App = () => {
               (showFinished || !item.isCompleted) && (
                 <div
                   key={item.id}
-                  className="todo p-2 w-[90%] md:w-[80%] bg-[#E1F7F5] flex justify-between my-1 rounded-md"
+                  className="flex flex-col w-[90%] items-center mb-1"
                 >
-                  <div className="flex items-center gap-2">
-                    <input
-                      id={item.id}
-                      name={item.id}
-                      type="checkbox"
-                      checked={item.isCompleted}
-                      onChange={(e) => {
-                        handlecheckbox(e);
-                      }}
-                      className="cursor-pointer"
-                    />
+                  <div className="todo px-2 py-1 w-[90%] md:w-[80%] bg-[#E1F7F5] flex justify-between rounded-t-md">
+                    <div className="flex items-center gap-2">
+                      <input
+                        id={item.id}
+                        name={item.id}
+                        type="checkbox"
+                        checked={item.isCompleted}
+                        onChange={(e) => {
+                          handlecheckbox(e);
+                        }}
+                        className="cursor-pointer"
+                      />
 
-                    <label
-                      htmlFor={item.id}
-                      className={
-                        item.isCompleted
-                          ? " flex justify-center items-center cursor-pointer md:text-lg font-semibold line-through"
-                          : " flex justify-center items-center cursor-pointer md:text-lg font-semibold"
-                      }
-                    >
-                      {item.todo}
-                    </label>
+                      <label
+                        htmlFor={item.id}
+                        className={
+                          item.isCompleted
+                            ? " flex justify-center items-center cursor-pointer md:text-lg font-semibold line-through"
+                            : " flex justify-center items-center cursor-pointer md:text-lg font-semibold"
+                        }
+                      >
+                        {item.todo}
+                      </label>
+                    </div>
+
+                    <div className="flex justify-center items-center gap-1 md:gap-2">
+                      <button
+                        name={item.id}
+                        onClick={() => {
+                          handleEdit(item.id);
+                        }}
+                        className="bg-slate-500 p-2 rounded-md font-semibold text-white"
+                      >
+                        <MdEditDocument />
+                      </button>
+                      <button
+                        name={item.id}
+                        onClick={(e) => {
+                          handleDelete(item.id);
+                        }}
+                        className="bg-red-500 p-2 rounded-md font-semibold text-white"
+                      >
+                        <RiDeleteBin6Fill />
+                      </button>
+                    </div>
                   </div>
-
-                  <div className="flex justify-center items-center gap-1 md:gap-2">
-                    <button
-                      name={item.id}
-                      onClick={() => {
-                        handleEdit(item.id);
-                      }}
-                      className="bg-slate-500 p-2 rounded-md font-semibold text-white"
-                    >
-                      <MdEditDocument />
-                    </button>
-                    <button
-                      name={item.id}
-                      onClick={(e) => {
-                        handleDelete(item.id);
-                      }}
-                      className="bg-red-500 p-2 rounded-md font-semibold text-white"
-                    >
-                      <RiDeleteBin6Fill />
-                    </button>
+                  <div className="flex flex-wrap justify-end gap-[2px] md:gap-1 p-[2px] md:px-2 md:py-1 bg-[#E1F7F5] w-[90%] md:w-[80%] rounded-b-md text-[12px] font-semibold">
+                    <div>
+                      {" "}
+                      Created at:{" "}
+                      <span className="text-blue-500">{item.createdAt}</span>
+                    </div>
+                    <div>
+                      {" "}
+                      Due date:{" "}
+                      <span className="text-blue-500">
+                        {item.duedate ? new Date(item.duedate).toLocaleString() : "No due date"}
+                      </span>
+                    </div>
                   </div>
                 </div>
               )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="flex justify-around items-center h-20 bg-[#1E0342] text-white">
-      <div className="logo text-2xl font-bold">iTask - <span className="text-xl font-semibold">Manage Your Tasks</span></div>
+      <div className="logo text-3xl font-bold">iTask - <span className="text-xl font-semibold">Manage Your Tasks</span></div>
       {/* <ul className="nav-right flex gap-20 text-xl">
         <li className="hover:font-bold cursor-pointer transition-all">Home</li>
         <li className="hover:font-bold cursor-pointer transition-all">About</li>
